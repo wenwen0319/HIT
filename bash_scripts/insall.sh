b38986e42871f06d9f129395cb38ecc2d69c8aae
wenwen0319 <291666792@qq.com>
2021-02-10 10:45:56 -0500

    Add files via upload

---
+++ bash_scripts/insall.sh
@@ -0,0 +1,8 @@
+git clone git@github.com:Abel0828/IDRL.git
+conda create --name newproject python=3.7.6
+conda activate newproject
+conda install scikit-learn
+conda install pandas
+conda install pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.0 -c pytorch
+conda install matplotlib
+pip install tqdm
